a403ca6c21787963d2a9d8a388034bf5474cdb96
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-11-03 22:25:53 +0100

    Send data to backend

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,10 +4,38 @@ const Users = ({ usersPromise }) => {
   const users = use(usersPromise);
   console.log(users);
 
+  const handleAddUser = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    console.log(name, email);
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("After Post", data);
+      });
+  };
+
   return (
     <div>
       <div>
-        <h3>Add a User</h3>\
+        <h3>Add a User</h3>
+        <form onSubmit={handleAddUser}>
+          <input type="text" name="name" id="name" placeholder="Name" />
+          <br />
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <br />
+          <button type="submit">Add User</button>
+        </form>
       </div>
       <div>
         {users.map((user) => (
